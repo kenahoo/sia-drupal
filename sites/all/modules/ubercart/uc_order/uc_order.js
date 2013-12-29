4ede2e1c03233c77d2d14ec0ed52be43a8f2b288
Ken Williams <kenahoo@gmail.com>
2013-12-28 23:05:48 -0600

    Upgrade ubercart module

--- sites/all/modules/ubercart/uc_order/uc_order.js
+++ sites/all/modules/ubercart/uc_order/uc_order.js
@@ -1,102 +1,70 @@
-
 /**
  * @file
  * Handles asynchronous requests for order editing forms.
  */
 
 var customer_select = '';
-var add_product_browser = '';
-var order_save_holds = 0;
 
 /**
- * Adds the double click behavior to the order table at admin/store/orders.
+ * Adds double click behavior to the order and customer admin tables.
  */
-Drupal.behaviors.ucOrderClick = function(context) {
-  $('.uc-orders-table tr.odd, .uc-orders-table tr.even:not(.ucOrderClick-processed)', context).addClass('ucOrderClick-processed').each(
-    function() {
-      $(this).dblclick(
-        function() {
-          var url = Drupal.settings.ucURL.adminOrders + this.id.substring(6);
-          window.location = url;
-        }
-      );
-    }
-  );
-}
-
-/**
- * Adds the submit behavior to the order form.
- */
-Drupal.behaviors.ucOrderSubmit = function(context) {
-  $('#uc-order-edit-form:not(.ucOrderSubmit-processed)', context).addClass('ucOrderSubmit-processed').submit(
-    function() {
-      $('#products-selector').empty().removeClass();
-      $('#delivery_address_select').empty().removeClass();
-      $('#billing_address_select').empty().removeClass();
-      $('#customer-select').empty().removeClass();
-    }
-  );
-}
-
-$(document).ready(
-  function() {
-    if (order_save_holds == 0) {
-      release_held_buttons();
-    }
-
-    $('.uc-orders-table tr.odd, .uc-orders-table tr.even').each(
+Drupal.behaviors.ucOrderClick = {
+  attach: function(context, settings) {
+    jQuery('.view-uc-orders tbody tr, .view-uc-customers tbody tr', context).dblclick(
       function() {
-        $(this).dblclick(
-          function() {
-            var url = Drupal.settings.ucURL.adminOrders + this.id.substring(6);
-            window.location = url;
-          }
-        );
-      }
-    );
-
-    $('#uc-order-edit-form').submit(
-      function() {
-        $('#products-selector').empty().removeClass();
-        $('#delivery_address_select').empty().removeClass();
-        $('#billing_address_select').empty().removeClass();
-        $('#customer-select').empty().removeClass();
+        window.location = jQuery(this).find('.views-field-order-id a').attr('href');
       }
     );
   }
-);
+}
 
 /**
- * Copys the shipping data on the order edit screen to the corresponding billing
- * fields if they exist.
+ * Adds the submit behavior to the order form
+ */
+Drupal.behaviors.ucOrderSubmit = {
+  attach: function(context, settings) {
+    jQuery('#uc-order-edit-form:not(.ucOrderSubmit-processed)', context).addClass('ucOrderSubmit-processed').submit(
+      function() {
+        jQuery('#products-selector').empty().removeClass();
+        jQuery('#delivery_address_select').empty().removeClass();
+        jQuery('#billing_address_select').empty().removeClass();
+        jQuery('#customer-select').empty().removeClass();
+      }
+    );
+  }
+}
+
+/**
+ * Copies the shipping data on the order edit screen to the corresponding
+ * billing fields if they exist.
  */
 function uc_order_copy_shipping_to_billing() {
-  if ($('#edit-delivery-zone').html() != $('#edit-billing-zone').html()) {
-    $('#edit-billing-zone').empty().append($('#edit-delivery-zone').children().clone());
+  if (jQuery('#edit-delivery-zone').html() != jQuery('#edit-billing-zone').html()) {
+    jQuery('#edit-billing-zone').empty().append(jQuery('#edit-delivery-zone').children().clone());
   }
 
-  $('#uc-order-edit-form input, select, textarea').each(
+  jQuery('#uc-order-edit-form input, select, textarea').each(
     function() {
       if (this.id.substring(0, 13) == 'edit-delivery') {
-        $('#edit-billing' + this.id.substring(13)).val($(this).val());
+        jQuery('#edit-billing' + this.id.substring(13)).val(jQuery(this).val());
       }
     }
   );
 }
 
 /**
- * Copys the billing data on the order edit screen to the corresponding shipping
- * fields if they exist.
+ * Copies the billing data on the order edit screen to the corresponding
+ * shipping fields if they exist.
  */
 function uc_order_copy_billing_to_shipping() {
-  if ($('#edit-billing-zone').html() != $('#edit-delivery-zone').html()) {
-    $('#edit-delivery-zone').empty().append($('#edit-billing-zone').children().clone());
+  if (jQuery('#edit-billing-zone').html() != jQuery('#edit-delivery-zone').html()) {
+    jQuery('#edit-delivery-zone').empty().append(jQuery('#edit-billing-zone').children().clone());
   }
 
-  $('#uc-order-edit-form input, select, textarea').each(
+  jQuery('#uc-order-edit-form input, select, textarea').each(
     function() {
       if (this.id.substring(0, 12) == 'edit-billing') {
-        $('#edit-delivery' + this.id.substring(12)).val($(this).val());
+        jQuery('#edit-delivery' + this.id.substring(12)).val(jQuery(this).val());
       }
     }
   );
@@ -112,10 +80,10 @@ function load_address_select(uid, div, address_type) {
     'func' : "apply_address('" + address_type + "', this.value);"
   };
 
-  $.post(Drupal.settings.ucURL.adminOrders + 'address_book', options,
-         function (contents) {
-           $(div).empty().addClass('address-select-box').append(contents);
-         }
+  jQuery.post(Drupal.settings.ucURL.adminOrders + 'address_book', options,
+    function (contents) {
+      jQuery(div).empty().addClass('address-select-box').append(contents);
+    }
   );
 }
 
@@ -124,31 +92,27 @@ function load_address_select(uid, div, address_type) {
  */
 function apply_address(type, address_str) {
   eval('var address = ' + address_str + ';');
-  $('#edit-' + type + '-first-name').val(address['first_name']);
-  $('#edit-' + type + '-last-name').val(address['last_name']);
-  $('#edit-' + type + '-phone').val(address['phone']);
-  $('#edit-' + type + '-company').val(address['company']);
-  $('#edit-' + type + '-street1').val(address['street1']);
-  $('#edit-' + type + '-street2').val(address['street2']);
-  $('#edit-' + type + '-city').val(address['city']);
-  $('#edit-' + type + '-postal-code').val(address['postal_code']);
+  jQuery('#edit-' + type + '-first-name').val(address['first_name']);
+  jQuery('#edit-' + type + '-last-name').val(address['last_name']);
+  jQuery('#edit-' + type + '-phone').val(address['phone']);
+  jQuery('#edit-' + type + '-company').val(address['company']);
+  jQuery('#edit-' + type + '-street1').val(address['street1']);
+  jQuery('#edit-' + type + '-street2').val(address['street2']);
+  jQuery('#edit-' + type + '-city').val(address['city']);
+  jQuery('#edit-' + type + '-postal-code').val(address['postal_code']);
 
-  if ($('#edit-' + type + '-country').val() != address['country']) {
-    $('#edit-' + type + '-country').val(address['country']);
-    try {
-      uc_update_zone_select('edit-' + type + '-country', address['zone']);
-    }
-    catch (err) {}
+  if (jQuery('#edit-' + type + '-country').val() != address['country']) {
+    jQuery('#edit-' + type + '-country').val(address['country']);
   }
 
-  $('#edit-' + type + '-zone').val(address['zone']);
+  jQuery('#edit-' + type + '-zone').val(address['zone']);
 }
 
 /**
  * Closes the address book div.
  */
 function close_address_select(div) {
-  $(div).empty().removeClass('address-select-box');
+  jQuery(div).empty().removeClass('address-select-box');
   return false;
 }
 
@@ -156,17 +120,17 @@ function close_address_select(div) {
  * Loads the customer select div on the order edit screen.
  */
 function load_customer_search() {
-  if (customer_select == 'search' && $('#customer-select #edit-back').val() == null) {
+  if (customer_select == 'search' && jQuery('#customer-select #edit-back').val() == null) {
     return close_customer_select();
   }
 
-  $.post(Drupal.settings.ucURL.adminOrders + 'customer', {},
-         function (contents) {
-           $('#customer-select').empty().addClass('customer-select-box').append(contents);
-           $('#customer-select #edit-first-name').val($('#edit-billing-first-name').val());
-           $('#customer-select #edit-last-name').val($('#edit-billing-last-name').val());
-           customer_select = 'search';
-         }
+  jQuery.post(Drupal.settings.ucURL.adminOrders + 'customer', {},
+    function (contents) {
+      jQuery('#customer-select').empty().addClass('customer-select-box').append(contents);
+      jQuery('#customer-select #edit-first-name').val(jQuery('#edit-billing-first-name').val());
+      jQuery('#customer-select #edit-last-name').val(jQuery('#edit-billing-last-name').val());
+      customer_select = 'search';
+    }
   );
 
   return false;
@@ -176,15 +140,14 @@ function load_customer_search() {
  * Displays the results of the customer search.
  */
 function load_customer_search_results() {
-  $.post(Drupal.settings.ucURL.adminOrders + 'customer/search',
+  jQuery.post(Drupal.settings.ucURL.adminOrders + 'customer/search',
     {
-      first_name: $('#customer-select #edit-first-name').val(),
-      last_name: $('#customer-select #edit-last-name').val(),
-      email: $('#customer-select #edit-email').val(),
-      username: $('#customer-select #edit-username').val()
+      first_name: jQuery('#customer-select #edit-first-name').val(),
+      last_name: jQuery('#customer-select #edit-last-name').val(),
+      email: jQuery('#customer-select #edit-email').val()
     },
     function (contents) {
-      $('#customer-select').empty().append(contents);
+      jQuery('#customer-select').empty().append(contents);
     }
   );
   return false;
@@ -194,17 +157,9 @@ function load_customer_search_results() {
  * Sets customer values from search selection.
  */
 function select_customer_search() {
-  var data = $('#edit-cust-select').val();
-  $('#edit-uid').val(data.substr(0, data.indexOf(':')));
-  $('#edit-uid-text').val(data.substr(0, data.indexOf(':')));
-  $('#edit-primary-email').val(data.substr(data.indexOf(':') + 1));
-  $('#edit-primary-email-text').val(data.substr(data.indexOf(':') + 1));
-  try {
-    $('#edit-submit-changes').get(0).click();
-  }
-  catch (err) {
-  }
-  return close_customer_select();
+  var data = jQuery('#edit-cust-select').val();
+  var i = data.indexOf(':');
+  return select_existing_customer(data.substr(0, i), data.substr(i + 1));
 }
 
 /**
@@ -215,11 +170,11 @@ function load_new_customer_form() {
     return close_customer_select();
   }
 
-  $.post(Drupal.settings.ucURL.adminOrders + 'customer/new', {},
-         function (contents) {
-           $('#customer-select').empty().addClass('customer-select-box').append(contents);
-           customer_select = 'new';
-         }
+  jQuery.post(Drupal.settings.ucURL.adminOrders + 'customer/new', {},
+    function (contents) {
+      jQuery('#customer-select').empty().addClass('customer-select-box').append(contents);
+      customer_select = 'new';
+    }
   );
   return false;
 }
@@ -229,13 +184,13 @@ function load_new_customer_form() {
  */
 function check_new_customer_address() {
   var options = {
-    'email' : $('#customer-select #edit-email').val(),
-    'sendmail' : $('#customer-select #edit-sendmail').attr('checked')
+    'email' : jQuery('#customer-select #edit-email').val(),
+    'sendmail' : jQuery('#customer-select #edit-sendmail').attr('checked')
   };
-  $.post(Drupal.settings.ucURL.adminOrders + 'customer/new/check', options,
-         function (contents) {
-           $('#customer-select').empty().append(contents);
-         }
+  jQuery.post(Drupal.settings.ucURL.adminOrders + 'customer/new/check', options,
+    function (contents) {
+      jQuery('#customer-select').empty().append(contents);
+    }
   );
   return false;
 }
@@ -244,12 +199,10 @@ function check_new_customer_address() {
  * Loads existing customer as new order's customer.
  */
 function select_existing_customer(uid, email) {
-  $('#edit-uid').val(uid);
-  $('#edit-uid-text').val(uid);
-  $('#edit-primary-email').val(email);
-  $('#edit-primary-email-text').val(email);
+  jQuery('input[name=uid], #edit-uid-text').val(uid);
+  jQuery('input[name=primary_email], #edit-primary-email-text').val(email);
   try {
-    $('#edit-submit-changes').click();
+    jQuery('#edit-submit-changes').click();
   }
   catch (err) {
   }
@@ -260,232 +213,7 @@ function select_existing_customer(uid, email) {
  * Hides the customer selection form.
  */
 function close_customer_select() {
-  $('#customer-select').empty().removeClass('customer-select-box');
+  jQuery('#customer-select').empty().removeClass('customer-select-box');
   customer_select = '';
   return false;
-}
-
-/**
- * Loads the products div on the order edit screen.
- */
-function uc_order_load_product_edit_div(order_id) {
-  $(document).ready(
-    function() {
-      add_order_save_hold();
-
-      show_product_throbber();
-
-      $.post(Drupal.settings.ucURL.adminOrders + order_id + '/products',
-             { action: 'view' },
-             function(contents) {
-               if (contents != '') {
-                 $('#products-container').empty().append(contents);
-               }
-               remove_order_save_hold();
-               hide_product_throbber();
-             });
-    }
-  );
-}
-
-/**
- * Loads the product selection form.
- */
-function load_product_select(order_id, search) {
-  if (search == true) {
-    options = {'search' : $('#edit-product-search').val()};
-  }
-  else {
-    options = { };
-  }
-
-  show_product_throbber();
-
-  $.post(Drupal.settings.ucURL.adminOrders + order_id + '/product_select', options,
-         function (contents) {
-           $('#products-selector').empty().addClass('product-select-box2').append(contents);
-           hide_product_throbber();
-         }
-  );
-
-  return false;
-}
-
-/**
- * Deprecated?
- */
-function select_product() {
-  add_product_form();
-  return false;
-}
-
-/**
- * Hides product selection form.
- */
-function close_product_select() {
-  $('#products-selector').empty().removeClass('product-select-box2');
-  return false;
-}
-
-/**
- * Loads the quantity and other extra product fields.
- */
-function add_product_form() {
-  add_product_browser = $('#products-selector').html();
-
-  show_product_throbber();
-
-  if (parseInt($('#edit-unid').val()) > 0) {
-    $.post(Drupal.settings.ucURL.adminOrders + $('#edit-order-id').val() + '/add_product/' + $('#edit-unid').val(), { },
-           function(contents) {
-             $('#products-selector').empty().append(contents);
-             hide_product_throbber();
-           }
-    );
-  }
-}
-
-/**
- * Adds the selected product to the order.
- */
-function add_product_to_order(order_id, node_id) {
-  var post_vars = fetch_product_data();
-  post_vars['action'] = 'add';
-  post_vars['nid'] = node_id;
-  post_vars['qty'] = $('#edit-add-qty').val();
-
-  $('#uc-order-add-product-form :input').not(':radio:not(:checked), :checkbox:not(:checked)').each(
-    function() {
-      post_vars[$(this).attr('name')] = $(this).val();
-    }
-  );
-
-  show_product_throbber();
-
-  $.post(Drupal.settings.ucURL.adminOrders + order_id + '/products', post_vars,
-         function(contents) {
-           if (contents != '') {
-             $('#products-container').empty().append(contents);
-           }
-           hide_product_throbber();
-         }
-  );
-
-  $('#add-product-button').click();
-
-  return false;
-}
-
-/**
- * Gathers all of the products' data fields into one array.
- */
-function fetch_product_data() {
-  var pdata = { };
-
-  $('#products-container :input').each(function() {
-    if (this.type == 'checkbox') {
-      if (this.checked) {
-        pdata[$(this).attr('name')] = $(this).val();
-      }
-    }
-    else {
-      pdata[$(this).attr('name')] = $(this).val();
-    }
-  });
-
-  return pdata;
-}
-
-/**
- * Button to create a new row of empty data fields.
- */
-function add_blank_line_button(order_id) {
-  var post_vars = fetch_product_data();
-  post_vars['action'] = 'add_blank';
-
-  show_product_throbber();
-
-  $.post(Drupal.settings.ucURL.adminOrders + order_id + '/products',
-         post_vars,
-         function(contents) {
-           if (contents != '') {
-             $('#products-container').empty().append(contents);
-           }
-           hide_product_throbber();
-         }
-  );
-}
-
-/**
- * Button to remove product from the order.
- */
-function remove_product_button(message, opid) {
-  if (confirm(message)) {
-    var post_vars = fetch_product_data();
-    post_vars['action'] = 'remove';
-    post_vars['opid'] = opid;
-
-    show_product_throbber();
-
-    $.post(Drupal.settings.ucURL.adminOrders + $('#edit-order-id').val() + '/products',
-           post_vars,
-           function(contents) {
-             if (contents != '') {
-               $('#products-container').empty().append(contents);
-             }
-             hide_product_throbber();
-           }
-    );
-  }
-}
-
-/**
- * Prevents mistakes by confirming deletion.
- */
-function confirm_line_item_delete(message, img_id) {
-  if (confirm(message)) {
-    var li_id = img_id.substring(3);
-    $('#edit-li-delete-id').val(li_id);
-    $('#uc-order-edit-form #edit-submit-changes').get(0).click();
-  }
-}
-
-/**
- * Disables order submit button while parts of the page are still loading.
- */
-function add_order_save_hold() {
-  order_save_holds++;
-  $('#uc-order-edit-form input.save-button').attr('disabled', 'disabled');
-}
-
-/**
- * Removes a hold and enable the save buttons when all holds are gone!
- */
-function remove_order_save_hold() {
-  order_save_holds--;
-
-  if (order_save_holds == 0) {
-    release_held_buttons();
-  }
-}
-
-/**
- * Removes the disable attribute on any input item with the save-button class.
- */
-function release_held_buttons() {
-  $('#uc-order-edit-form input.save-button').removeAttr('disabled');
-}
-
-/**
- * User feedback that something is happening.
- */
-function show_product_throbber() {
-  $('#product-div-throbber').attr('style', 'background-image: url(' + Drupal.settings.basePath + 'misc/throbber.gif); background-repeat: no-repeat; background-position: 100% -20px;').html('&nbsp;&nbsp;&nbsp;&nbsp;');
-}
-
-/**
- * Done loading forms.
- */
-function hide_product_throbber() {
-  $('#product-div-throbber').removeAttr('style').empty();
 }

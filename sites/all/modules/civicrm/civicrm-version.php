2337c4464f211d2d92919fc34047bf494e3a7603
Ken Williams <kenahoo@gmail.com>
2014-01-01 17:22:42 -0600

    Upgrade civicrm module

--- sites/all/modules/civicrm/civicrm-version.php
+++ sites/all/modules/civicrm/civicrm-version.php
@@ -1,7 +1,7 @@
 <?php
 function civicrmVersion( ) {
   return array( 'version'  => '4.4.3',
-                'cms'      => 'Drupal6',
+                'cms'      => 'Drupal',
                 'revision' => '6994c77d3f' );
 }
 

897ad3006ad0fdc8f1f07b2bd5c9b1d42c739e7f
Ken Williams <kenahoo@gmail.com>
2014-01-10 20:54:21 -0600

    Don't need templates_c dir (see http://forum.civicrm.org/index.php?topic=15430.0)

--- sites/default/files/civicrm/templates_c/en_US/%%F0/F0F/F0F71C33%%version.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-08-07 20:20:12
-         compiled from CRM/common/version.tpl */ ?>
-4.2.8
--- sites/default/files/civicrm/templates_c/en_US/%%F5/F5F/F5FF6485%%Summary.hlp.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-08-07 20:23:48
-         compiled from CRM/Contact/Page/View/Summary.hlp */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('block', 'htxt', 'CRM/Contact/Page/View/Summary.hlp', 26, false),array('block', 'ts', 'CRM/Contact/Page/View/Summary.hlp', 27, false),)), $this); ?>
-<?php $this->_tag_stack[] = array('htxt', array('id' => "id-next-prev-buttons")); $_block_repeat=true;smarty_block_htxt($this->_tag_stack[count($this->_tag_stack)-1][1], null, $this, $_block_repeat);while ($_block_repeat) { ob_start(); ?>
-<?php $this->_tag_stack[] = array('ts', array()); $_block_repeat=true;smarty_block_ts($this->_tag_stack[count($this->_tag_stack)-1][1], null, $this, $_block_repeat);while ($_block_repeat) { ob_start(); ?>Next/Previous navigation buttons are not available for searches which are sorted by email, phone or address or fields.<?php $_block_content = ob_get_contents(); ob_end_clean(); $_block_repeat=false;echo smarty_block_ts($this->_tag_stack[count($this->_tag_stack)-1][1], $_block_content, $this, $_block_repeat); }  array_pop($this->_tag_stack); ?>
-<?php $_block_content = ob_get_contents(); ob_end_clean(); $_block_repeat=false;echo smarty_block_htxt($this->_tag_stack[count($this->_tag_stack)-1][1], $_block_content, $this, $_block_repeat); }  array_pop($this->_tag_stack); ?>

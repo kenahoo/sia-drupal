ad8a82910e1c7e6778430a5e38f622d56b5847de
Ken Williams <kenahoo@gmail.com>
2014-03-25 18:04:03 -0500

    Use the right database params on the right machine

--- sites/default/db.php
+++ sites/default/db.php
@@ -1,17 +1,40 @@
 <?php
 
-$databases = array (
-  'default' =>
-  array (
-    'default' =>
-    array (
-      'driver' => 'mysql',
-      'database' => 'pantheon3',
-      'username' => 'kenahoo',
-      'password' => '***REMOVED***',
-      'host' => 'mysql.singersinaccord.org',
-      'port' => '',
-      'prefix' => '',
-    ),
-  ),
-);
+global $databases;
+
+if (preg_match("{^/Users/ken/Documents/SIA/site-upgrade}", $_SERVER["DOCUMENT_ROOT"])) {
+
+    $databases = array (
+        'default' =>
+        array (
+            'default' =>
+            array (
+                'driver' => 'mysql',
+                'database' => 'pantheon',
+                'username' => 'ken',
+                'password' => '***REMOVED***',
+                'host' => 'localhost',
+                'port' => '',
+                'prefix' => '',
+            ),
+        ),
+    );
+
+} else {
+
+    $databases = array (
+        'default' =>
+        array (
+            'default' =>
+            array (
+                'driver' => 'mysql',
+                'database' => 'pantheon3',
+                'username' => 'kenahoo',
+                'password' => '***REMOVED***',
+                'host' => 'mysql.singersinaccord.org',
+                'port' => '',
+                'prefix' => '',
+            ),
+        ),
+    );
+}

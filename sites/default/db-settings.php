84e868dbf76de93b25e244f4cca6a527ea8833ac
Ken Williams <kenahoo@gmail.com>
2016-02-26 17:31:28 -0600

    Read DB connection info from .my.cnf

--- sites/default/db-settings.php
+++ sites/default/db-settings.php
@@ -4,67 +4,41 @@ global $databases;
 global $site_root;
 global $host;
 
+$databases = array (
+    'default' =>
+    array (
+        'default' =>
+        array (
+            'driver' => 'mysql',
+            'port' => '',
+            'prefix' => '',
+        ),
+    ),
+);
+
+
 if (preg_match("{^/Users/ken/Documents/SIA/site-upgrade}", getenv("DOCUMENT_ROOT"))
     ||
     preg_match("{^/Users/ken/Documents/SIA/site-upgrade}", getenv("PWD"))) {
 
     $host = 'localhost';
     $site_root = '/Users/ken/Documents/SIA/site-upgrade';
-    $databases = array (
-        'default' =>
-        array (
-            'default' =>
-            array (
-                'driver' => 'mysql',
-                'database' => 'pantheon',
-                'username' => 'ken',
-                'password' => '***REMOVED***',
-                'host' => 'localhost',
-                'port' => '',
-                'prefix' => '',
-            ),
-        ),
-    );
-
-} else if (preg_match("{dev.singersinaccord.org}", getenv("DOCUMENT_ROOT"))
-           ||
-           preg_match("{dev.singersinaccord.org}", getenv("PWD"))) {
-
-    $host = 'dev.singersinaccord.org';
-    $site_root = '/home/kenahoo/dev.singersinaccord.org';
-    $databases = array (
-        'default' =>
-        array (
-            'default' =>
-            array (
-                'driver' => 'mysql',
-                'database' => 'dev_sia',
-                'username' => 'kenahoo',
-                'password' => '***REMOVED***',
-                'host' => 'mysql.singersinaccord.org',
-                'port' => '',
-                'prefix' => '',
-            ),
-        ),
-    );
 
 } else {
-
     $host = 'www.singersinaccord.org';
     $site_root = '/home/kenahoo/singersinaccord.org';
-    $databases = array (
-        'default' =>
-        array (
-            'default' =>
-            array (
-                'driver' => 'mysql',
-                'database' => 'pantheon3',
-                'username' => 'kenahoo',
-                'password' => '***REMOVED***',
-                'host' => 'mysql.singersinaccord.org',
-                'port' => '',
-                'prefix' => '',
-            ),
-        ),
-    );
 }
+
+function flerb($dbs) {
+    $shell_user = posix_getpwuid(posix_getuid());
+    $cnf = parse_ini_file($shell_user['dir']."/.my.cnf");
+    foreach(['password', 'database', 'host', 'user'] as $key) {
+        $key2 = $key=='user' ? 'username' : $key;
+        if (isset($cnf[$key])) {
+            $dbs[$key2] = $cnf[$key];
+        }
+    }
+    return $dbs;
+}
+
+$databases['default']['default'] = flerb($databases['default']['default']);

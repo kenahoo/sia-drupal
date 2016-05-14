517c097ffab3c2fd3f53525ce41fad2fdc421e57
Ken Williams <kenahoo@gmail.com>
2016-05-13 23:02:51 -0500

    More changes for sureserver

--- sites/default/civicrm.settings.php
+++ sites/default/civicrm.settings.php
@@ -1,90 +1,83 @@
 <?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.7                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2015                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
 /**
- * CiviCRM Configuration File - v2.2
+ * CiviCRM Configuration File.
  */
 
 /**
  * Content Management System (CMS) Host:
  *
- * CiviCRM can be hosted in either Drupal or Joomla.
- * 
+ * CiviCRM can be hosted in either Drupal 6 or 7, Joomla or WordPress.
+ *
+ * Settings for Backdrop CMS:
+ *      define( 'CIVICRM_UF'        , 'Backdrop');
+ *
+ * Settings for Drupal 7.x:
+ *      define( 'CIVICRM_UF'        , 'Drupal');
+ *
  * Settings for Drupal 6.x:
- *      define( 'CIVICRM_UF'        , 'Drupal' );
+ *      define( 'CIVICRM_UF'        , 'Drupal6');
  *
- * Settings for 1.5.x:
- *      define( 'CIVICRM_UF'        , 'Joomla' );
+ * Settings for Joomla 1.7.x - 2.5.x:
+ *      define( 'CIVICRM_UF'        , 'Joomla');
  *
- * Settings for Standalone:
- *		define( 'CIVICRM_UF'	, 'Standalone');
+ * Settings for WordPress 3.3.x:
+ *      define( 'CIVICRM_UF'        , 'WordPress');
  *
  * You may have issues with images in CiviCRM. If this is the case, be sure
- * to update the CiviCRM Resource URL field (in Administer CRM: Global
- * Settings: Resource URLS) to your CiviCRM root directory.
+ * to update the CiviCRM Resource URL field to your CiviCRM root directory
+ * (Administer::System Settings::Resource URLs).
  */
-define( 'CIVICRM_UF'               , 'Drupal'        );
-
-/**
- * Pantheon Systems:
- *
- * Repopulate needed variables based on the Pantheon environment if applicable.
- * http://www.kalamuna.com/news/civicrm-pantheon
- *
- */
-if (!empty($_SERVER['PRESSFLOW_SETTINGS'])) {
-  $env = json_decode($_SERVER['PRESSFLOW_SETTINGS'], TRUE);
-  if (!empty($env['conf']['pantheon_binding'])) {
-    $pantheon_db = $env['databases']['default']['default'];
-    $pantheon_conf = $env['conf'];
-
-    //user name and password
-    $db_string = $pantheon_db['driver'] . '://' . $pantheon_db['username'] . ':' . $pantheon_db['password'] . '@';
-    //host
-    $db_string .= 'dbserver.' . $pantheon_conf['pantheon_environment'] . '.' . $pantheon_conf['pantheon_site_uuid'] . '.drush.in' . ':' . $pantheon_db['port'];
-    // database
-    $db_string .= '/' . $pantheon_db['database'] . '?new_link=true';
-
-    // define the database strings
-    define('CIVICRM_UF_DSN', $db_string);
-    define('CIVICRM_DSN', $db_string);
-
-    // define the file paths
-    global $civicrm_root;
-
-    //$civicrm_root = '/srv/bindings/' . $pantheon_conf['pantheon_binding'] . '/code/' . str_replace('drupal', '', drupal_get_path('module', 'civicrm'));
-    $civicrm_root = '/srv/bindings/' . $pantheon_conf['pantheon_binding'] . '/code/sites/all/modules/civicrm';
-    define('CIVICRM_TEMPLATE_COMPILEDIR', '/srv/bindings/' . $pantheon_conf['pantheon_binding'] . '/files/civicrm/templates_c/');
-
-    // Use Drupal base url and path
-    global $base_url, $base_path;
-    define('CIVICRM_UF_BASEURL', $base_url . '/');
-    define( 'CIVICRM_IDS_ENABLE', 0);
-    define( 'CIVICRM_SITE_KEY', '2391f678908b90245f6306c04d3e3b09' );
-  
-    if ( $pantheon_conf['pantheon_environment'] == 'dev' || $pantheon_conf['pantheon_environment'] == 'test' ){ 
-      /**
-      * This setting logs all emails to a file. Useful for debugging any mail (or civimail) issues.
-      * This will not send any email, so ensure this is commented out in production
-      */
-     define( 'CIVICRM_MAIL_LOG', '/srv/bindings/' . $pantheon_conf['pantheon_binding'] . '/code/sites/default/files/civicrm/templates_c/mail.log' );
-    }  
+if (!defined('CIVICRM_UF')) {
+  if (getenv('CIVICRM_UF')) {
+    define('CIVICRM_UF', getenv('CIVICRM_UF'));
   }
-} else {
+  else {
+    define('CIVICRM_UF', 'Drupal');
+  }
+}
 
 /**
  * Content Management System (CMS) Datasource:
  *
- * Update this setting with your CMS (Drupal or Joomla) database username, server and DB name. Comment it out if using CiviCRM standalone.
+ * Update this setting with your CMS (Drupal, Backdrop CMS, or Joomla) database username, server and DB name.
  * Datasource (DSN) format:
  *      define( 'CIVICRM_UF_DSN', 'mysql://cms_db_username:cms_db_password@db_server/cms_database?new_link=true');
  */
-
-global $databases;
-global $site_root;
-global $civicrm_root;
-global $host;
-require_once 'db-settings.php';
-$d = $databases['default']['default'];
-define( 'CIVICRM_UF_DSN'           , "mysql://$d[username]:$d[password]@$d[host]/$d[database]?new_link=true" );
+if (!defined('CIVICRM_UF_DSN') && CIVICRM_UF !== 'UnitTests') {
+  global $databases;
+  global $site_root;
+  global $civicrm_root;
+  global $host;
+  require_once 'db-settings.php';
+  $d = $databases['default']['default'];
+  define( 'CIVICRM_UF_DSN'           , "mysql://$d[username]:$d[password]@$d[host]/$d[database]?new_link=true" );
+}
 
 /**
  * CiviCRM Database Settings
@@ -94,18 +87,42 @@ define( 'CIVICRM_UF_DSN'           , "mysql://$d[username]:$d[password]@$d[host]
  *      define( 'CIVICRM_DSN', 'mysql://crm_db_username:crm_db_password@db_server/crm_database?new_link=true');
  *
  * Drupal and CiviCRM can share the same database, or can be installed into separate databases.
+ * Backdrop CMS and CiviCRM can also share the same database, or can be installed into separate databases.
  *
- * EXAMPLE: Drupal and CiviCRM running in the same database...
- *      DB Name = drupal, DB User = drupal
- *      define( 'CIVICRM_DSN'         , 'mysql://drupal:YOUR_PASSWORD@localhost/drupal?new_link=true' );
+ * EXAMPLE: Drupal/Backdrop and CiviCRM running in the same database...
+ *      DB Name = cms, DB User = cms
+ *      define( 'CIVICRM_DSN'         , 'mysql://cms:YOUR_PASSWORD@localhost/cms?new_link=true');
  *
- * EXAMPLE: Drupal and CiviCRM running in separate databases...
- *      Drupal  DB Name = drupal, DB User = drupal
+ * EXAMPLE: Drupal/Backdrop and CiviCRM running in separate databases...
+ *      CMS DB Name = cms, DB User = cms
  *      CiviCRM DB Name = civicrm, CiviCRM DB User = civicrm
- *      define( 'CIVICRM_DSN'         , 'mysql://civicrm:YOUR_PASSWORD@localhost/civicrm?new_link=true' );
+ *      define( 'CIVICRM_DSN'         , 'mysql://civicrm:YOUR_PASSWORD@localhost/civicrm?new_link=true');
  *
  */
-define( 'CIVICRM_DSN'           , "mysql://$d[username]:$d[password]@$d[host]/$d[database]?new_link=true" );
+if (!defined('CIVICRM_DSN')) {
+  if (CIVICRM_UF === 'UnitTests' && isset($GLOBALS['_CV']['TEST_DB_DSN'])) {
+    define('CIVICRM_DSN', $GLOBALS['_CV']['TEST_DB_DSN']);
+  }
+  else {
+    global $databases;
+    global $site_root;
+    global $civicrm_root;
+    global $host;
+    require_once 'db-settings.php';
+    $d = $databases['default']['default'];
+    define( 'CIVICRM_DSN', "mysql://$d[username]:$d[password]@$d[host]/$d[database]?new_link=true" );
+  }
+}
+
+/**
+ * CiviCRM Logging Database
+ *
+ * Used to point to a different database to use for logging (if desired). If unset defaults to equal CIVICRM_DSN.
+ * The CIVICRM_DSN user needs to have the rights to modify the below database schema and be able to write to it.
+ */
+if (!defined('CIVICRM_LOGGING_DSN')) {
+  define('CIVICRM_LOGGING_DSN', CIVICRM_DSN);
+}
 
 /**
  * File System Paths:
@@ -121,13 +138,21 @@ define( 'CIVICRM_DSN'           , "mysql://$d[username]:$d[password]@$d[host]/$d
  * and your web server must have read/write access to these directories.
  *
  *
- * EXAMPLE - CivicSpace / Drupal:
- * If the path to the CivicSpace or Drupal home directory is /var/www/htdocs/civicspace
+ * EXAMPLE - Drupal:
+ * If the path to the Drupal home directory is /var/www/htdocs/drupal
  * the $civicrm_root setting would be:
- *      $civicrm_root = '/var/www/htdocs/civicspace/modules/civicrm/';
+ *      $civicrm_root = '/var/www/htdocs/drupal/sites/all/modules/civicrm/';
  *
  * the CIVICRM_TEMPLATE_COMPILEDIR would be:
- *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/civicspace/files/civicrm/templates_c/' );
+ *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/drupal/sites/default/files/civicrm/templates_c/');
+ *
+ * EXAMPLE - Backdrop CMS:
+ * If the path to the Backdrop home directory is /var/www/htdocs/backdrop
+ * the $civicrm_root setting would be:
+ *      $civicrm_root = '/var/www/htdocs/backdrop/modules/civicrm/';
+ *
+ * the CIVICRM_TEMPLATE_COMPILEDIR would be:
+ *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/backdrop/files/civicrm/templates_c/');
  *
  * EXAMPLE - Joomla Installations:
  * If the path to the Joomla home directory is /var/www/htdocs/joomla
@@ -135,15 +160,16 @@ define( 'CIVICRM_DSN'           , "mysql://$d[username]:$d[password]@$d[host]/$d
  *      $civicrm_root = '/var/www/htdocs/joomla/administrator/components/com_civicrm/civicrm/';
  *
  * the CIVICRM_TEMPLATE_COMPILEDIR would be:
- *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/joomla/media/civicrm/templates_c/' );
+ *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/joomla/media/civicrm/templates_c/');
  *
- * EXAMPLE - Standalone Installations:
- * If the path to the Standalone home directory is /var/www/htdocs/civicrm
+ * EXAMPLE - WordPress Installations:
+ * If the path to the WordPress home directory is /var/www/htdocs/wordpress and the path to the plugin directory is /var/www/htdocs/wordpress/wp-content/plugins
  * the $civicrm_root setting would be:
- *      $civicrm_root = '/var/www/htdocs/civicrm/';
+ *      $civicrm_root = '/var/www/htdocs/wordpress/wp-content/plugins/civicrm/civicrm/';
  *
- * the CIVICRM_TEMPLATE_COMPILEDIR might be:
- *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/civicrm/standalone/files/templates_c/' );
+ * the CIVICRM_TEMPLATE_COMPILEDIR would be:
+ *      define( 'CIVICRM_TEMPLATE_COMPILEDIR', '/var/www/htdocs/wordpress/wp-content/uploads/civicrm/templates_c/');
+ *
  */
 
 global $civicrm_root;
@@ -154,40 +180,86 @@ define( 'CIVICRM_TEMPLATE_COMPILEDIR', $site_root . '/sites/default/files/civicr
 /**
  * Site URLs:
  *
- * This section defines absolute and relative URLs to access the host CMS (Drupal or Joomla) resources.
+ * This section defines absolute and relative URLs to access the host CMS (Backdrop, Drupal, or Joomla) resources.
  *
  * IMPORTANT: Trailing slashes should be used on all URL settings.
- * 
+ *
  *
  * EXAMPLE - Drupal Installations:
  * If your site's home url is http://www.example.com/drupal/
- * these variables would be set as below. Modify as needed for your install. 
+ * these variables would be set as below. Modify as needed for your install.
  *
  * CIVICRM_UF_BASEURL - home URL for your site:
- *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/drupal/' );
+ *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/drupal/');
+ *
+ * EXAMPLE - Backdrop CMS Installations:
+ * If your site's home url is http://www.example.com/backdrop/
+ * these variables would be set as below. Modify as needed for your install.
+ *
+ * CIVICRM_UF_BASEURL - home URL for your site:
+ *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/backdrop/');
  *
  * EXAMPLE - Joomla Installations:
  * If your site's home url is http://www.example.com/joomla/
  *
  * CIVICRM_UF_BASEURL - home URL for your site:
  * Administration site:
- *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/joomla/administrator/' );
+ *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/joomla/administrator/');
  * Front-end site:
- *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/joomla/' );
+ *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/joomla/');
  *
  * EXAMPLE - Standalone Installations:
  * If your site's home url is http://www.example.com/civicrm/
  *
- * CIVICRM_UF_BASEURL - home URL for your site:
- *      define( 'CIVICRM_UF_BASEURL' , 'http://www.example.com/civicrm/standalone/' );
+ * Uncomment and edit the below as appropriate.
  */
  
 define( 'CIVICRM_UF_BASEURL'      , 'http://'.$host.'/' );
 
-/*
+ // Override the Temporary Files directory.
+ // $civicrm_setting['Directory Preferences']['customFileUploadDir'] = '/path/to/upload';
+
+ // Override the custom files upload directory.
+ // $civicrm_setting['Directory Preferences']['uploadDir'] = '/path/to/upload-dir' ;
+
+ // Override the images directory.
+ // $civicrm_setting['Directory Preferences']['imageUploadDir'] = '/path/to/image-upload-dir' ;
+
+ // Override the custom templates directory.
+ // $civicrm_setting['Directory Preferences']['customTemplateDir'] = '/path/to/template-dir';
+
+ // Override the Custom php path directory.
+ // $civicrm_setting['Directory Preferences']['customPHPPathDir'] = '/path/to/custom-php-dir';
+
+ // Override the extensions directory.
+ // $civicrm_setting['Directory Preferences']['extensionsDir'] = '/path/to/extensions-dir';
+
+ // Override the resource url
+ // $civicrm_setting['URL Preferences']['userFrameworkResourceURL'] = 'http://example.com/example-resource-url/';
+
+ // Override the Image Upload URL (System Settings > Resource URLs)
+ // $civicrm_setting['URL Preferences']['imageUploadURL'] = 'http://example.com/example-image-upload-url';
+
+ // Override the Custom CiviCRM CSS URL
+ // $civicrm_setting['URL Preferences']['customCSSURL'] = 'http://example.com/example-css-url' ;
+
+ // Override the extensions resource URL
+ // $civicrm_setting['URL Preferences']['extensionsURL'] = 'http://example.com/pathtoextensiondir'
+
+ // Disable display of Community Messages on home dashboard
+ // $civicrm_setting['CiviCRM Preferences']['communityMessagesUrl'] = false;
+
+ // Disable automatic download / installation of extensions
+ // $civicrm_setting['Extension Preferences']['ext_repo_url'] = false;
+
+ // Override the CMS root path defined by cmsRootPath.
+ // define('CIVICRM_CMSDIR', '/path/to/install/root/');
+
+
+/**
  * If you are using any CiviCRM script in the bin directory that
  * requires authentication, then you also need to set this key.
- * We recommend using a 16-32 bit alphanumeric/punctuation key. 
+ * We recommend using a 16-32 bit alphanumeric/punctuation key.
  * More info at http://wiki.civicrm.org/confluence/display/CRMDOC/Command-line+Script+Configuration
  */
 define( 'CIVICRM_SITE_KEY', null );
@@ -200,8 +272,8 @@ define( 'CIVICRM_SITE_KEY', null );
  *
  */
 
-$include_path = '.'        . PATH_SEPARATOR .
-                $civicrm_root . PATH_SEPARATOR . 
+$include_path = '.'           . PATH_SEPARATOR .
+                $civicrm_root . PATH_SEPARATOR .
                 $civicrm_root . DIRECTORY_SEPARATOR . 'packages' . PATH_SEPARATOR .
                 get_include_path( );
 set_include_path( $include_path );
@@ -215,7 +287,7 @@ if ( function_exists( 'variable_get' ) && variable_get('clean_url', '0') != '0' 
 // force PHP to auto-detect Mac line endings
 ini_set('auto_detect_line_endings', '1');
 
-// make sure the memory_limit is at least 48 MB
+// make sure the memory_limit is at least 64 MB
 $memLimitString = trim(ini_get('memory_limit'));
 $memLimitUnit   = strtolower(substr($memLimitString, -1));
 $memLimit       = (int) $memLimitString;
@@ -227,5 +299,6 @@ switch ($memLimitUnit) {
 if ( $memLimit >= 0 and $memLimit < 50331648 ) {
     ini_set('memory_limit', '48M');
 }
+
 require_once 'CRM/Core/ClassLoader.php';
 CRM_Core_ClassLoader::singleton()->register();
